2c93f795d89b6ba94a7c5708e0b614860484a8fb
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-04 23:59:40 +0700

    thuc hanh tinh chi so bmi

---
+++ src/IntroductionToJava/CheckYourBMI.java
@@ -0,0 +1,28 @@
+package IntroductionToJava;
+
+import java.util.Scanner;
+
+public class CheckYourBMI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your weight? ");
+        double weight = scanner.nextDouble();
+
+        System.out.print("What is your height? ");
+        double height = scanner.nextDouble();
+
+        double bmi = weight/(height * height);
+        System.out.printf("Your BMI is %f \n",bmi);
+
+        if (bmi < 18.5) {
+            System.out.print("You are Underweight");
+        } else if (bmi < 25) {
+            System.out.print("You are Normal");
+        } else if (bmi < 30) {
+            System.out.print("You are Overweight");
+        } else {
+            System.out.print("You are Obese");
+        }
+    }
+}
