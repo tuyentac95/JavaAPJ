53ea58ef9fe75046dec8acacd2067e30a6605773
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-05 11:15:16 +0700

    bai tap doi USD sang VND

---
+++ src/IntroductionToJava/ConvertUSDToVND.java
@@ -0,0 +1,18 @@
+package IntroductionToJava;
+
+import java.util.Scanner;
+
+public class ConvertUSDToVND {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the exchange rate of USD/VND: ");
+        double usd_vnd = scanner.nextDouble();
+
+        System.out.print("Enter USD: ");
+        double usd = scanner.nextDouble();
+
+        double vnd = usd * usd_vnd;
+        System.out.printf("%.2f USD is equal to %.2f",usd,vnd);
+    }
+}
