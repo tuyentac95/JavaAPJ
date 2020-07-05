9596446cc598203a4ee942f38f572d1267a508a0
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-05 09:46:02 +0700

    thuc hanh hien thi loi chao

---
+++ src/IntroductionToJava/Greeting.java
@@ -0,0 +1,14 @@
+package IntroductionToJava;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.printf("Hello %s!",name);
+    }
+}
