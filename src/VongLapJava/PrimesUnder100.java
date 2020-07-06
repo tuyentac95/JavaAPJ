6218e72dd361180deafc734708816d02801d9f0c
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-06 23:42:04 +0700

    bai tap hien thi so nguyen to nho hon 100

---
+++ src/VongLapJava/PrimesUnder100.java
@@ -0,0 +1,25 @@
+package VongLapJava;
+
+import java.util.Scanner;
+
+public class PrimesUnder100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the limit: ");
+        int limit = scanner.nextInt();
+
+        for (int i=2; i<limit; i++) {
+            boolean isPrime = true;
+            for (int j=2; j<=Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
