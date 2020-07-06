1b31fe9ab2de85e45a291efea5040935255ff3dd
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-06 08:51:08 +0700

    thuc hanh kiem tra so nguyen to

---
+++ src/VongLapJava/CheckPrime.java
@@ -0,0 +1,31 @@
+package VongLapJava;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your number: ");
+        int number = scanner.nextInt();
+        boolean isPrime;
+
+        if (number < 2) {
+            isPrime = false;
+        } else {
+            isPrime = true;
+            for (int i=2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.printf("%d is a prime",number);
+        } else {
+            System.out.printf("%d is not a prime", number);
+        }
+    }
+}
