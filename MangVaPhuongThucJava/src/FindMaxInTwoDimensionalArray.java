8d86aa16588fbc38f7891dc4fc7308e334771377
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-07 23:58:15 +0700

    bai tap tim max trong mang 2 chieu

---
+++ MangVaPhuongThucJava/src/FindMaxInTwoDimensionalArray.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class FindMaxInTwoDimensionalArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row, col;
+        do {
+            System.out.print("Enter the rows of matrix: ");
+            row = sc.nextInt();
+            if (row > 5) {
+                System.out.println("The number of rows should be smaller!");
+            }
+        } while (row > 5);
+
+        do {
+            System.out.print("Enter the columns of matrix: ");
+            col = sc.nextInt();
+            if (col > 5) {
+                System.out.println("The number of rows should be smaller!");
+            }
+        } while (col > 5);
+
+        double[][] matrix = new double[row][col];
+        for (int i=0; i < matrix.length; i++) {
+            for (int j=0; j < matrix[i].length; j++) {
+                System.out.print("Enter the element at position (" + i + ", " + j + ") : ");
+                matrix[i][j] = sc.nextDouble();
+            }
+        }
+
+        for (int i=0; i < matrix.length; i++) {
+            for (int j=0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "   ");
+            }
+            System.out.print("\n");
+        }
+
+        double matrixMax = matrix[0][0];
+        int rowOfMax = 0, colOfMax = 0;
+        for (int i=0; i < matrix.length; i++) {
+            for (int j=0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > matrixMax) {
+                    matrixMax = matrix[i][j];
+                    rowOfMax = i;
+                    colOfMax = j;
+                }
+            }
+        }
+
+        System.out.println("The largest element in matrix is " + matrixMax + " in position (" + rowOfMax + ", " + colOfMax + ")");
+    }
+}
