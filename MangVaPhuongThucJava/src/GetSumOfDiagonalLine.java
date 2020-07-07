6779053b45ec2c304a3efe51772a3c69dda58440
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-08 00:22:28 +0700

    bai tap tinh tong duong cheo chinh trong ma tran

---
+++ MangVaPhuongThucJava/src/GetSumOfDiagonalLine.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class GetSumOfDiagonalLine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size;
+        do {
+            System.out.print("Enter the size of matrix: ");
+            size = sc.nextInt();
+            if (size > 5) {
+                System.out.println("The size of matrix should be smaller!");
+            }
+        } while (size > 5);
+
+        double[][] matrix = new double[size][size];
+        for (int i=0; i < matrix.length; i++) {
+            for (int j=0; j < matrix[i].length; j++) {
+                System.out.print("Enter the element at position (" + i + ", " + j + ") : ");
+                matrix[i][j] = sc.nextDouble();
+            }
+        }
+
+        for (int i=0; i < matrix.length; i++) {
+            for (int j=0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "   ");
+            }
+            System.out.print("\n");
+        }
+
+        double sumDiagonal = 0;
+        for (int i=0; i < matrix.length; i++) {
+            sumDiagonal += matrix[i][i];
+        }
+        System.out.println("The sum of the main diagonal is " + sumDiagonal);
+    }
+}
