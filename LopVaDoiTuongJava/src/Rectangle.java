65a26c0aba9556bca5ba67686a31d18a43b46fc5
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-08 11:22:41 +0700

    thuc hanh lop hinh chu nhat

---
+++ LopVaDoiTuongJava/src/Rectangle.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height: ");
+        double height = scanner.nextDouble();
+
+        Rectangle rectangle = new Rectangle(width, height);
+
+        System.out.println("Your rectangle:\n" + rectangle.display());
+        System.out.println("Perimeter of your rectangle: " + rectangle.getPerimeter());
+        System.out.println("Area of your rectangle: " + rectangle.getArea());
+    }
+
+    double width, height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    public String display() {
+        return "Rectangle{width = " + width + ", height = " + height + "}";
+    }
+}
