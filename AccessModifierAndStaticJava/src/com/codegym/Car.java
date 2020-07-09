c8c5d9de7da4794b02ac9e251cd2a53cb055d1c3
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-09 14:30:40 +0700

    Static Property

---
+++ AccessModifierAndStaticJava/src/com/codegym/Car.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class Car {
+    private String name;
+
+    private String engine;
+
+    public static int numberOfCars;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+}
