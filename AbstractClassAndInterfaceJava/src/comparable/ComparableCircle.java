4ebee238aafad5626f74e099e4f40d5d624b5738
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-13 17:01:36 +0700

    thuc hanh comparable circle

---
+++ AbstractClassAndInterfaceJava/src/comparable/ComparableCircle.java
@@ -0,0 +1,23 @@
+package comparable;
+
+import comparator.Circle;
+
+public class ComparableCircle extends Circle implements Comparable <ComparableCircle>{
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color) {
+        super(radius,color);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 0;
+    }
+}
--- AbstractClassAndInterfaceJava/src/comparator/Circle.java
+++ AbstractClassAndInterfaceJava/src/comparator/Circle.java
@@ -4,14 +4,14 @@ public class Circle {
     private double radius = 1.0;
     private String color = "red";
 
-    Circle() {
+    public Circle() {
     }
 
-    Circle(double radius) {
+    public Circle(double radius) {
         this.radius = radius;
     }
 
-    Circle(double radius, String color) {
+    public Circle(double radius, String color) {
         this.radius = radius;
         this.color = color;
     }
