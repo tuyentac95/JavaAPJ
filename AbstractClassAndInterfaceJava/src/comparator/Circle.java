85734b1b9ac30ad1410b2b203d87a16d2a14fde4
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-13 16:39:14 +0700

    thuc hanh comparator

---
+++ AbstractClassAndInterfaceJava/src/comparator/Circle.java
@@ -0,0 +1,34 @@
+package comparator;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    Circle() {
+    }
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
