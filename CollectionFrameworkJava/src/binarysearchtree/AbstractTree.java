884274c7f9074880541188a5e0c99c260a7c5ec6
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-07-26 21:07:25 +0700

    thuc hanh binary search tree

---
+++ CollectionFrameworkJava/src/binarysearchtree/AbstractTree.java
@@ -0,0 +1,7 @@
+package binarysearchtree;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+    @Override
+    public void inorder() {
+    }
+}
---
+++ CollectionFrameworkJava/src/binarysearchtree/Tree.java
@@ -0,0 +1,7 @@
+package binarysearchtree;
+
+public interface Tree<E> {
+    public boolean insert(E e);
+    public void inorder();
+    public int getSize();
+}
